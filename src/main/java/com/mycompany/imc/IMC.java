ade552a5d4a464469253434443a8f653d9bf0f30
matbena <crosserain@gmail.com>
2025-05-26 16:56:43 -0300

    initial commit

---
+++ src/main/java/com/mycompany/imc/IMC.java
@@ -0,0 +1,12 @@
+package com.mycompany.imc;
+import igu.Pantalla;
+
+public class IMC {
+      public static void main(String[] args) {
+        
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+    }
+}
